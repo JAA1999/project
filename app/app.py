9e7f2444a1466ef173bdd9439b1426b6cd8bd948
Jamie Allan <jamiealexanderallan@outlook.com>
2022-04-08 11:09:58 +0000

    Update to be in line with pythonAnywhere

--- app/app.py
+++ app/app.py
@@ -1,18 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask import render_template
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Text"
     return render_template("base.html")
 
 
-@app.route("/update_model", methods=['POST'])
-def update_model():
-    model_file = request._get_file_stream()
-    with open("app/static/model.gltf", 'rb') as f:
-        f.write(model_file.read())
-    return "", 204
+@app.route("/upload-file", methods=['GET', 'POST'])
+def upload_file():
+    print(f"Entered upload\n{request.method}", flush=True)
+    if request.method == 'GET':
+        return os.getcwd()
+    if request.method == 'POST':
+        print('Entered post request', flush=True)
+        model = request.files['model']
+        binary = request.files['binary']
+        model.save(os.path.join('./mysite/project/app/static', secure_filename(model.filename)))
+        binary.save(os.path.join('./mysite/project/app/static', secure_filename(binary.filename)))
+        return Response(status=200)
